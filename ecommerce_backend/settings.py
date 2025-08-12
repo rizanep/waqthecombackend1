db2322d0f078f6eb4d205672533647bf7e4c0d92
rizanep <khaderrizan@gmail.com>
2025-08-12 17:09:34 +0530

    ensdv

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # React's local dev server
+    "http://localhost:5173", '3.111.227.204', # React's local dev server
 ]
 REST_FRAMEWORK = {"EXCEPTION_HANDLER": "rest_framework.views.exception_handler"}
 MIDDLEWARE = [
