b86f86e8f380db0649ddeeec1df6fb376f4aa23f
rizanep <khaderrizan@gmail.com>
2025-08-13 11:10:47 +0530

    static

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -153,8 +153,8 @@ REST_FRAMEWORK = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
